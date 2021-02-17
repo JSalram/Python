b288c21ed1aed29e7b1a846f5520c725405155f5
Javi <javisr997@gmail.com>
2021-02-17 13:45:12 +0100

    Django

--- Django/blog_project/blog_project/urls.py
+++ Django/blog_project/blog_project/urls.py
@@ -1,4 +1,4 @@
-"""pages_project URL Configuration
+"""blog_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -11,12 +11,12 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog_project/', include('blog_project.urls'))
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
+    path('', include('blog.urls')),
 ]
